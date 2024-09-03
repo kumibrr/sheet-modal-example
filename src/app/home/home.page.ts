0d3a7f670b113c957e6c796b6bb901f610470d12
Israel de la Barrera <isra.lora99@gmail.com>
2024-09-03 11:19:46 +0000

    Added example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonModal, IonItem, IonAvatar, IonImg, IonLabel, IonSearchbar, IonList
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonModal,  IonItem, IonAvatar, IonImg, IonLabel, IonSearchbar, IonList],
 })
 export class HomePage {
   constructor() {}
